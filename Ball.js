435a72b6860bd6dc7c5eb518ddcacd62f54ea98e
yunnii <yunniihi@gmail.com>
2012-12-02 23:38:06 +0600

    Отталкивание от стен JSlint

--- Ball.js
+++ Ball.js
@@ -11,41 +11,48 @@
     "use strict";
 
     var Ball = function (paper, x, y, r) {
+        this.r = r;
         this.circle = paper.circle(x, y, r).attr({fill: "#66e", "stroke-width": 2});
         this.areaRadius = r + 30;
     };
 
+    function getCircleToPath(x, y, r) {
+        var s = "M";
+        s = s + (x) + "," + (y - r) + "A" + r + "," + r + ",0,1,1," + (x - 0.1) + "," + (y - r) + "z";
+        return s;
+    }
+
     exports.Ball = Ball;
 
-    Ball.prototype.SetXY = function(x, y) {
-            this.circle.attrs.cx = x;
-            this.circle.attrs.cy = y;
-        }
+    Ball.prototype.SetXY = function (x, y) {
+        this.circle.attrs.cx = x;
+        this.circle.attrs.cy = y;
+    };
 
-    Ball.prototype.X = function() { return this.circle.attrs.cx;}
-    Ball.prototype.Y = function() { return this.circle.attrs.cy;}
+    Ball.prototype.X = function () { return this.circle.attrs.cx; };
+    Ball.prototype.Y = function () { return this.circle.attrs.cy; };
+    Ball.prototype.circleString = function () { return getCircleToPath(this.X(), this.Y(), this.r); };
+
+    var speed = 50;
 
     Ball.prototype.moveBall = function (x, y) {
 
-        var center = {X : this.X(), Y: this.Y()};
-        
-        var tangens = (x - center.X === 0) ? 0 : (y - center.Y) / (x - center.X),
+        var center = {X : this.X(), Y: this.Y()},
+            tangens = (x - center.X === 0) ? 0 : (y - center.Y) / (x - center.X),
             nextX,
             nextY;
 
-        if(x - center.X === 0) {
+        if (x - center.X === 0) {
             nextX = center.X;
-            nextY = (y - center.Y > 0 ) ? center.Y - 20 :
-                    center.Y + 20;
-            console.log("d");
-        }
-        else{
-            nextX = (x - center.X > 0) ? center.X - 20 :
-                    center.X + 20;
+            nextY = (y - center.Y > 0) ? center.Y - speed :
+                    center.Y + speed;
+        } else {
+            nextX = (x - center.X > 0) ? center.X - speed :
+                    center.X + speed;
             nextY = tangens * nextX + (center.Y - tangens * center.X);
-            if (Math.abs(tangens) > 1.1){
-                nextY = (y - center.Y > 0 ) ? center.Y - 20 :
-                    center.Y + 20;
+            if (Math.abs(tangens) > 1.1) {
+                nextY = (y - center.Y > 0) ? center.Y - speed :
+                        center.Y + speed;
             }
         }
 
@@ -57,4 +64,21 @@
         return (Math.abs(this.X() - x) < this.areaRadius &&
                 Math.abs(this.Y() - y) < this.areaRadius);
     };
+                    // ищем уравнение прямой, перпендикулярной стенке, о которую ударились
+                    // находим точку, симметричную центру относительно данной прямой
+    Ball.prototype.findReflectionPoint = function (hitX, hitY, tangens) {
+        var center = {X : this.X(), Y: this.Y()},
+            revertTangens = (tangens === 0) ? 0 : 1 / tangens;
+
+        var x = (tangens === 0) ? hitX : (tangens / (tangens * tangens + 1)) * (hitY - center.Y + tangens * center.X + hitX * revertTangens);
+        var y = (tangens === 0) ? center.Y :  tangens * x + (center.Y - tangens * center.X);
+
+        var resultX = 2 * x - center.X,
+            resultY = 2 * y - center.Y;
+
+        var pushX = 2 * hitX - resultX,
+            pushY = 2 * hitY - resultY;
+
+        return {x : pushX, y: pushY};
+    }
 }(window));
--- animate.js
+++ animate.js
@@ -10,28 +10,88 @@
 /*global Raphael: true*/
 /*global model: true*/
 /*global Ball: true*/
+/*global tmpl: true*/
 (function (exports) {
     "use strict";
 
    // exports.R = R;
+    var template = "M <%= x1 %>,<%= y1 %> L <%= x2 %>, <%= y2 %> L <%= x1 %>,<%= y1 %>",
+        el,
+        start = null,
+        idInterval,
+        pathArray,
+        borderArray;
 
-    function load(border) {
+    function intersectChecker() {
+        setInterval(function () {
+            var i,
+                bbox,
+                intersect,
+                hit,
+                nextPoint,
+                tan;
+
+            for (i = 0; i < pathArray.length; i += 1) {
+
+                intersect = Raphael.pathIntersection(pathArray[i], el.circleString());
+                if (intersect.length > 0) {
+                    clearInterval(idInterval);
+
+                    hit = { x: 0, y: 0};
+                    intersect.forEach(function (el) {
+                        hit.x += el.x;
+                        hit.y += el.y;
+                    });
+                    hit.x /= intersect.length;
+                    hit.y /= intersect.length;
+
+                    bbox = borderArray[i].getBBox();
+                    tan = (bbox.x - bbox.x2 === 0) ? 0 : (bbox.y - bbox.y2) / (bbox.x - bbox.x2);
+
+                    nextPoint = el.findReflectionPoint(hit.x, hit.y, tan);
+
+                    el.moveBall(nextPoint.x, nextPoint.y);
+                    idInterval = setInterval(function () {
+                        el.moveBall(nextPoint.x, nextPoint.y);
+                    }, 1000);
+                }
+            }
+        }, 100);
+    }
+
+    function moveBall(e) {
+        if (start === null) {
+            start = 1;
+            intersectChecker();
+        }
+        e = e || window.event;
+
+        if (el.isNearBall(e.pageX, e.pageY)) {
+            clearInterval(idInterval);
+            el.moveBall(e.pageX, e.pageY);
+            idInterval = setInterval(function () {
+                el.moveBall(e.pageX, e.pageY);
+            }, 1000);
+
+            e.isImmediatePropagationStopped();
+        }
+    }
+
+    function load() {
 
         var colour = "r(0.5,0.5)hsb(4.8, 1, 1)-hsb(4.8, 1, .3)",
             canvas = Raphael("main", 1000, 1000);
 
-        canvas.path(border).attr({fill: "#ddd", "fill-opacity": 1, stroke: "#333", "stroke-width": 24});
-        var el = new Ball(canvas, 260, 350, 30);
-
-        $("#main").mousemove(function (e) {
-            e = e || window.event;
-
-            if (el.isNearBall(e.pageX, e.pageY))
-            {
-                 el.moveBall(e.pageX, e.pageY);
-                 e.isImmediatePropagationStopped()
-            }
+        pathArray.forEach(function (path) {
+            canvas.path(path)
+                        .attr({fill: "#ddd", "fill-opacity": 1, stroke: "#fff", "stroke-width": 16});
+            borderArray.push(canvas.path(path)
+                        .attr({fill: "#ddd", "fill-opacity": 1, stroke: "#333", "stroke-width": 12}));
         });
+
+        el = new Ball(canvas, 260, 350, 30);
+
+        $("#main").mousemove(moveBall);
     }
 
     function initialise() {
@@ -42,15 +102,21 @@
                 if (result.length === 0) {
                     return;
                 }
-                var border = "M " + result[0].x + "," + result[0].y,
-                    i;
 
-                for (i = 1; i < result.length; i += 1) {
-                    border += "L " + result[i].x + "," + result[i].y;
+                var i;
+
+                for (i = 0; i < result.length - 1; i += 1) {
+                    pathArray.push(tmpl(template, { x1: result[i].x,
+                                                    y1: result[i].y,
+                                                    x2: result[i + 1].x,
+                                                    y2: result[i + 1].y}));
                 }
-                border += "L " + result[0].x + "," + result[0].y;
+                pathArray.push(tmpl(template, { x1: result[result.length - 1].x,
+                                                y1: result[result.length - 1].y,
+                                                x2: result[0].x,
+                                                y2: result[0].y}));
 
-                load(border);
+                load();
             });
     }
 
